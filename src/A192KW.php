cddb478b6809a844a5ea98f50941616dc588c7d4
Spomky <Spomky@users.noreply.github.com>
2016-03-03 08:19:38 +0100

    Update A192KW.php

--- src/A192KW.php
+++ src/A192KW.php
@@ -17,13 +17,9 @@ class A192KW
 
     /**
      * @param string $kek The Key Encryption Key
-     *
-     * @throws \InvalidArgumentException If the size of the KEK is invalid
      */
     protected static function checkKEKSize($kek)
     {
-        if (strlen($kek) !== 24) {
-            throw new \InvalidArgumentException('Bad KEK size');
-        }
+        Assertion::eq(strlen($kek), 24, 'Bad KEK size');
     }
 }
