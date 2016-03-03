da088b4bc12a11a3900694a28d0be96e64deebf4
Spomky <Spomky@users.noreply.github.com>
2016-03-03 08:20:03 +0100

    Update A256KW.php

--- src/A256KW.php
+++ src/A256KW.php
@@ -17,13 +17,9 @@ class A256KW
 
     /**
      * @param string $kek The Key Encryption Key
-     *
-     * @throws \InvalidArgumentException If the size of the KEK is invalid
      */
     protected static function checkKEKSize($kek)
     {
-        if (strlen($kek) !== 32) {
-            throw new \InvalidArgumentException('Bad KEK size');
-        }
+        Assertion::eq(strlen($kek), 32, 'Bad KEK size');
     }
 }
